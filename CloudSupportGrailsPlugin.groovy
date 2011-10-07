277ba75d1dfea319a7a5687dafcfe72062fb4c4e
Burt Beckwith <burt@burtbeckwith.com>
2011-10-07 02:02:06 -0400

    1.0.7 release

--- CloudSupportGrailsPlugin.groovy
+++ CloudSupportGrailsPlugin.groovy
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 class CloudSupportGrailsPlugin {
-	String version = '1.0.6'
+	String version = '1.0.7'
 	String grailsVersion = '1.3.3 > *'
 	String author = 'Burt Beckwith'
 	String authorEmail = 'beckwithb@vmware.com'
